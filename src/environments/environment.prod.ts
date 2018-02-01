6e289f0a64ca92a1f295c2b1e113710e15f34f6f
Gilberto Stankiewicz <hello@kiewic.com>
2018-01-31 21:21:03 -0800

    Add properties of firebase setting in environment.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
+    apiKey: "paste your firebase apiKey here",
+    authDomain: "paste your firebase authDomain here",
+    databaseURL: "paste your firebase databaseURL here",
+    projectId: "paste your firebase projectId here",
+    storageBucket: "paste your firebase storageBucket here",
+    messagingSenderId: "paste your firebase messagingSenderId here"
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,5 +6,11 @@
 export const environment = {
   production: false,
   firebase: {
+    apiKey: "paste your firebase apiKey here",
+    authDomain: "paste your firebase authDomain here",
+    databaseURL: "paste your firebase databaseURL here",
+    projectId: "paste your firebase projectId here",
+    storageBucket: "paste your firebase storageBucket here",
+    messagingSenderId: "paste your firebase messagingSenderId here"
   }
 };
